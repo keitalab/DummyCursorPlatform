8c8c17eeed0d645e1af6c1df8ce6f54527724961
ogapy <kyon6690@gmail.com>
2020-10-19 22:12:01 +0900

    SettingsとEvent(設定用スクリプト)を追加

---
+++ Assets/Scripts/StartScene/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    // 練習かどうか
+    public static bool isPractice = true;
+    void Start()
+    {
+        
+    }
+
+    public static void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_STANDALONE
+            UnityEngine.Application.Quit();
+        #endif
+    }
+
+}
