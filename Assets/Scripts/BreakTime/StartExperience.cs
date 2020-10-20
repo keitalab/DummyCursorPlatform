f7daccdd9e1928abcd52f5ba258b691ec6a275e0
ogapy <kyon6690@gmail.com>
2020-10-20 19:54:22 +0900

    Experienceへの遷移

--- Assets/Scripts/BreakTime/StartExperience.cs
+++ Assets/Scripts/BreakTime/StartExperience.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartExperience : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public class StartExperience : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        SceneManager.LoadScene("CountDown");
     }
 }
