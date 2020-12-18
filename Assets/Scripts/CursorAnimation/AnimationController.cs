0399987ad8a00657d8e29494f25f8939bfee8080
ogapy <kyon6690@gmail.com>
2020-12-18 16:01:23 +0900

    動画をgifとして生成

--- Assets/Scripts/CursorAnimation/AnimationController.cs
+++ Assets/Scripts/CursorAnimation/AnimationController.cs
@@ -1,25 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Moments;
 
 public class AnimationController : MonoBehaviour
 {
     public GameObject cursor;
     public GameObject background;
-    int index;
+    public static int index;
+    public Recorder rec;
     void Start()
     {
         cursor.transform.localScale = new Vector2(SelectFile.LogDiameter * 10, SelectFile.LogDiameter * 10);
         background.transform.localScale = new Vector2(SelectFile.LogWindowSize / 4, SelectFile.LogWindowSize / 4);
         Camera.main.orthographicSize = Screen.height / 2;
         cursor = Instantiate(cursor, new Vector3(0f, 0f, 0f), Quaternion.identity);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if(index < SelectFile.count)
-        cursor.transform.position = new Vector3(SelectFile.LogCursorX[index], SelectFile.LogCursorY[index], 0f);
-        index++;
+        {
+            cursor.transform.position = new Vector3(SelectFile.LogCursorX[index], SelectFile.LogCursorY[index], 0f);
+            index++;
+        }
+        else 
+        {
+            // rec.Save();
+        }
     }
 }
