9e1990f5818d173e8c3ac30897c7add7d1474deb
ogapy <kyon6690@gmail.com>
2021-02-18 21:33:37 +0900

    カーソルIDの大きさを調整

--- Assets/Scripts/CursorAnimation/AnimationController.cs
+++ Assets/Scripts/CursorAnimation/AnimationController.cs
@@ -5,30 +5,30 @@ using Moments;
 
 public class AnimationController : MonoBehaviour
 {
-    public GameObject cursor;
-    public GameObject background;
-    public static int index;
-    public Recorder rec;
-    void Start()
-    {
-        cursor.transform.localScale = new Vector2(SelectFile.LogDiameter, SelectFile.LogDiameter);
-        background.transform.localScale = new Vector2(SelectFile.LogWindowSize / 4, SelectFile.LogWindowSize / 4);
-        Camera.main.orthographicSize = Screen.height / 2;
-        cursor = Instantiate(cursor, new Vector3(0f, 0f, 0f), Quaternion.identity);
+  public GameObject cursor;
+  public GameObject background;
+  public static int index;
+  public Recorder rec;
+  void Start()
+  {
+    cursor.transform.localScale = new Vector2(SelectFile.LogDiameter * 5, SelectFile.LogDiameter * 5);
+    background.transform.localScale = new Vector2(SelectFile.LogWindowSize / 4, SelectFile.LogWindowSize / 4);
+    Camera.main.orthographicSize = Screen.height / 2;
+    cursor = Instantiate(cursor, new Vector3(0f, 0f, 0f), Quaternion.identity);
 
-    }
+  }
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+    if (index < SelectFile.count)
     {
-        if(index < SelectFile.count)
-        {
-            cursor.transform.position = new Vector3(SelectFile.LogCursorX[index], SelectFile.LogCursorY[index], 0f);
-            index++;
-        }
-        else 
-        {
-            // rec.Save();
-        }
+      cursor.transform.position = new Vector3(SelectFile.LogCursorX[index], SelectFile.LogCursorY[index], 0f);
+      index++;
     }
+    else
+    {
+      // rec.Save();
+    }
+  }
 }
